d38c6129f212f17e812f7dee98890823609dc1c6
omkarshinde254 <omkarshinde254@gmail.com>
2023-04-16 14:55:39 -0400

    Download Inception v3 and image net classes name

---
+++ classify.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+from PIL import Image
+from imageio import imread
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import tf_slim as slim
+from tf_slim.nets import inception
+import tf_slim as slim
+import cv2
+import matplotlib.pyplot as plt
+
